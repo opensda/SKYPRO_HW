55289db26e5ebbb0c08e64be32dcd37f8f8161ee
Sudarev_D <89365265993@mail.ru>
2023-06-02 16:17:14 +0300

    add is_title(line) with docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,12 @@
 def is_upper(line):
     new_line = line.upper()
     return new_line
+
+
+def is_title(line):
+    """
+    Делает все слова с заглавной буквы
+    """
+    new_line = line.title()
+    return new_line
+
